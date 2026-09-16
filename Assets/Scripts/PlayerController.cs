deab3e5b4aecc35b3cee3f59bbc9ef772bbe00df
TariqAlothman <s202274140@kfupm.edu.sa>
2026-09-16 11:11:18 +0300

    Added Reset High Score

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
-
 public class PlayerController : MonoBehaviour
 {
     private float elapsedTime = 0f;
     private float score = 0f;
     private float scoreMultiplier = 10f;
+    private float highScore = 0f;
     public float thrustForce = 0.25f;
     Rigidbody2D rb;
     public GameObject boosterFlame;
     public UIDocument uiDocument;
     private Label scoreText;
+    private Label highScoreText;
     private Button restartButton;
+    private Button resetHighScoreButton;
     public GameObject explosionEffect;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -23,9 +25,19 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         scoreText = uiDocument.rootVisualElement.Q<Label>("ScoreLabel");
+
+        highScoreText = uiDocument.rootVisualElement.Q<Label>("HighScore");
+        highScoreText.style.display = DisplayStyle.None;
+        highScore = PlayerPrefs.GetFloat("HighScore");
+
+
+        resetHighScoreButton = uiDocument.rootVisualElement.Q<Button>("ResetHighScore");
+        resetHighScoreButton.style.display = DisplayStyle.None;
+
         restartButton = uiDocument.rootVisualElement.Q<Button>("RestartButton");
         restartButton.style.display = DisplayStyle.None;
         restartButton.clicked += ReloadScene;
+        resetHighScoreButton.clicked += ResetHighScore;
     }
 
     // Update is called once per frame
@@ -43,10 +55,27 @@ public class PlayerController : MonoBehaviour
         Destroy(gameObject);
         Instantiate(explosionEffect, transform.position, transform.rotation);
         restartButton.style.display = DisplayStyle.Flex;
+
+
+        
+        if (score> highScore){
+            PlayerPrefs.SetFloat("HighScore",score);
+            highScore = score;
+        }
+        highScoreText.text = "High Score: " + highScore;
+        highScoreText.style.display = DisplayStyle.Flex;
+        resetHighScoreButton.style.display = DisplayStyle.Flex;
+
+
     }
     void ReloadScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    void ResetHighScore()
+    {
+        PlayerPrefs.SetFloat("HighScore",0f);
+        highScoreText.text = "High Score: " + 0;
     }
 
     void UpdateScore()
